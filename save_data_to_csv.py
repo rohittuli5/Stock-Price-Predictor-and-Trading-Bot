659201b188391c12cd9e727063384b2ddba865e4
Rohit Tuli <rohittuli08@gmail.com>
2020-08-19 14:31:24 +0530

    Initial Commit

---
+++ save_data_to_csv.py
@@ -0,0 +1,17 @@
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+
+def save_dataset(symbol, time_window):
+    api_key='XCTJNRLFF1V2W6HV'
+    print(symbol, time_window)
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, meta_data = ts.get_daily(symbol, outputsize='full')
+
+
+    pprint(data.head(10))
+
+    data.to_csv(f'./{symbol}_{time_window}.csv')
+
+
+if __name__ == "__main__":
+    save_dataset('MSFT','daily')
