1a635a34dafdca564a8c39e9e2241477441edf62
Rohit Tuli <rohittuli08@gmail.com>
2020-08-19 17:20:56 +0530

    Bug Fix

--- save_data_to_csv.py
+++ save_data_to_csv.py
@@ -2,7 +2,7 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 
 def save_dataset(symbol, time_window):
-    api_key='XCTJNRLFF1V2W6HV'
+    api_key=''
     print(symbol, time_window)
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol, outputsize='full')
