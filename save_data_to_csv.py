30fb0eddbd49997211c3dac2c99c036f89a27a3a
Rohit Tuli <rohittuli08@gmail.com>
2020-09-09 00:33:33 +0530

    Files Refresh

--- save_data_to_csv.py
+++ save_data_to_csv.py
@@ -6,12 +6,8 @@ def save_dataset(symbol, time_window):
     print(symbol, time_window)
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol, outputsize='full')
-
-
     pprint(data.head(10))
-
     data.to_csv(f'./{symbol}_{time_window}.csv')
-
 
 if __name__ == "__main__":
     save_dataset('MSFT','daily')
